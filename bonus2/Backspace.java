de2e2f8ca9c7aae4d8f74c9f6c0b2739dc6c6e4d
Jumana Suleiman <148403239+JumanaCS@users.noreply.github.com>
2023-10-18 22:05:15 -0500

    bonus2

---
+++ bonus2/Backspace.java
@@ -0,0 +1,15 @@
+/**
+* Given a string S containing letters and ‘#‘. The ‘#” represents a backspace. The task is to print the new string without ‘#‘.
+* Input : S = "abc#de#f#ghi#jklmn#op#"
+* Output : abdghjklmo
+
+* Input:  s = "ab##"
+* output: ""
+*/
+
+
+public class Backspace {
+    public static String erase(String str) {
+        
+    }
+}
